3159a6e2cebfa807ba6159b5e5f1618d46ec72cf
unknown <you@example.com>
2020-07-07 19:27:25 +0700

    Update Giao dien 07/07 - 10/07

---
+++ GiaoDienCafe_New/FormGiaoDien.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GiaoDienCafe_New
+{
+    public partial class FormGiaodien : Form
+    {
+        public FormGiaodien()
+        {
+            InitializeComponent();
+        }
+
+        private void FormGiaodien_Load(object sender, EventArgs e)
+        {
+            Guna.UI.Lib.GraphicsHelper.ShadowForm(this);
+        }
+
+        private void gunaPictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void gunaLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
