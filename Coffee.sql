4c60e01108c40c49c64b37671a24ec7cbc9bc570
unknown <you@example.com>
2020-07-10 18:27:59 +0700

    Update Giao dien va CSDL ngay 10/07

---
+++ Coffee.sql
@@ -0,0 +1,420 @@
+﻿
+USE [coffee]
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Account](
+	[UserName] [nvarchar](100) NOT NULL,
+	[DisplayName] [nvarchar](100) NOT NULL DEFAULT (N'Nhomdoi'),
+	[PassWord] [nvarchar](1000) NOT NULL DEFAULT ((0)),
+	[Type] [int] NOT NULL DEFAULT ((0)),
+PRIMARY KEY CLUSTERED 
+(
+	[UserName] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Bill](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[DateCheckIn] [date] NOT NULL DEFAULT (getdate()),
+	[DateCheckOut] [date] NULL,
+	[idTable] [int] NOT NULL,
+	[status] [int] NOT NULL DEFAULT ((0)),
+	[discount] [int] NULL,
+	[totalPrice] [float] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[BillInfo](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[idBill] [int] NOT NULL,
+	[idFood] [int] NOT NULL,
+	[count] [int] NOT NULL DEFAULT ((0)),
+PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Food](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[name] [nvarchar](100) NOT NULL DEFAULT (N'Chưa đặt tên'),
+	[idCategory] [int] NOT NULL,
+	[price] [float] NOT NULL DEFAULT ((0)),
+PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[FoodCategory](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[name] [nvarchar](100) NOT NULL DEFAULT (N'Chưa đặt tên'),
+PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[TableFood](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[name] [nvarchar](100) NOT NULL DEFAULT (N'Bàn chưa có tên'),
+	[status] [nvarchar](100) NOT NULL DEFAULT (N'Trống'),
+PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+INSERT [dbo].[Account] ([UserName], [DisplayName], [PassWord], [Type]) VALUES (N'admin', N'admin', N'123456', 1)
+INSERT [dbo].[Account] ([UserName], [DisplayName], [PassWord], [Type]) VALUES (N'staff', N'staff', N'123456', 0)
+SET IDENTITY_INSERT [dbo].[Bill] ON 
+
+INSERT [dbo].[Bill] ([id], [DateCheckIn], [DateCheckOut], [idTable], [status], [discount], [totalPrice]) VALUES (56, CAST(N'2018-01-17' AS Date), CAST(N'2018-01-17' AS Date), 7, 1, 20, 359200)
+INSERT [dbo].[Bill] ([id], [DateCheckIn], [DateCheckOut], [idTable], [status], [discount], [totalPrice]) VALUES (57, CAST(N'2018-01-17' AS Date), CAST(N'2018-01-17' AS Date), 12, 1, 20, 120000)
+INSERT [dbo].[Bill] ([id], [DateCheckIn], [DateCheckOut], [idTable], [status], [discount], [totalPrice]) VALUES (58, CAST(N'2018-01-17' AS Date), CAST(N'2018-01-17' AS Date), 9, 1, 10, 2637000)
+INSERT [dbo].[Bill] ([id], [DateCheckIn], [DateCheckOut], [idTable], [status], [discount], [totalPrice]) VALUES (59, CAST(N'2018-01-17' AS Date), CAST(N'2018-01-17' AS Date), 3, 1, 0, 39000)
+INSERT [dbo].[Bill] ([id], [DateCheckIn], [DateCheckOut], [idTable], [status], [discount], [totalPrice]) VALUES (60, CAST(N'2018-01-17' AS Date), CAST(N'2018-01-17' AS Date), 3, 1, 0, 39000)
+INSERT [dbo].[Bill] ([id], [DateCheckIn], [DateCheckOut], [idTable], [status], [discount], [totalPrice]) VALUES (61, CAST(N'2018-01-17' AS Date), CAST(N'2018-01-17' AS Date), 3, 1, 0, 180000)
+INSERT [dbo].[Bill] ([id], [DateCheckIn], [DateCheckOut], [idTable], [status], [discount], [totalPrice]) VALUES (62, CAST(N'2018-01-17' AS Date), NULL, 17, 0, NULL, NULL)
+SET IDENTITY_INSERT [dbo].[Bill] OFF
+SET IDENTITY_INSERT [dbo].[BillInfo] ON 
+
+INSERT [dbo].[BillInfo] ([id], [idBill], [idFood], [count]) VALUES (1, 56, 3, 1)
+INSERT [dbo].[BillInfo] ([id], [idBill], [idFood], [count]) VALUES (2, 56, 4, 3)
+INSERT [dbo].[BillInfo] ([id], [idBill], [idFood], [count]) VALUES (3, 56, 1, 1)
+INSERT [dbo].[BillInfo] ([id], [idBill], [idFood], [count]) VALUES (4, 56, 2, 1)
+INSERT [dbo].[BillInfo] ([id], [idBill], [idFood], [count]) VALUES (5, 57, 2, 1)
+INSERT [dbo].[BillInfo] ([id], [idBill], [idFood], [count]) VALUES (6, 58, 7, 10)
+INSERT [dbo].[BillInfo] ([id], [idBill], [idFood], [count]) VALUES (7, 58, 5, 10)
+INSERT [dbo].[BillInfo] ([id], [idBill], [idFood], [count]) VALUES (8, 58, 6, 1)
+INSERT [dbo].[BillInfo] ([id], [idBill], [idFood], [count]) VALUES (9, 59, 3, 1)
+INSERT [dbo].[BillInfo] ([id], [idBill], [idFood], [count]) VALUES (10, 60, 3, 1)
+INSERT [dbo].[BillInfo] ([id], [idBill], [idFood], [count]) VALUES (11, 62, 6, 1)
+SET IDENTITY_INSERT [dbo].[BillInfo] OFF
+SET IDENTITY_INSERT [dbo].[Food] ON 
+
+INSERT [dbo].[Food] ([id], [name], [idCategory], [price]) VALUES (1, N'Cà phê sữa', 2, 200000)
+INSERT [dbo].[Food] ([id], [name], [idCategory], [price]) VALUES (2, N'Cà phê đen', 2, 150000)
+INSERT [dbo].[Food] ([id], [name], [idCategory], [price]) VALUES (3, N'Sting', 1, 39000)
+INSERT [dbo].[Food] ([id], [name], [idCategory], [price]) VALUES (4, N'Coca', 1, 20000)
+INSERT [dbo].[Food] ([id], [name], [idCategory], [price]) VALUES (5, N'Trà ô long', 3, 250000)
+INSERT [dbo].[Food] ([id], [name], [idCategory], [price]) VALUES (6, N'Trà đào', 3, 180000)
+INSERT [dbo].[Food] ([id], [name], [idCategory], [price]) VALUES (7, N'Bia Heniken', 4, 25000)
+INSERT [dbo].[Food] ([id], [name], [idCategory], [price]) VALUES (8, N'Nước khoáng', 4, 2000)
+
+SET IDENTITY_INSERT [dbo].[Food] OFF
+SET IDENTITY_INSERT [dbo].[FoodCategory] ON 
+
+INSERT [dbo].[FoodCategory] ([id], [name]) VALUES (1, N'Nước ngọt')
+INSERT [dbo].[FoodCategory] ([id], [name]) VALUES (2, N'Cafe')
+INSERT [dbo].[FoodCategory] ([id], [name]) VALUES (3, N'Trà')
+INSERT [dbo].[FoodCategory] ([id], [name]) VALUES (4, N'Loại khác')
+SET IDENTITY_INSERT [dbo].[FoodCategory] OFF
+SET IDENTITY_INSERT [dbo].[TableFood] ON 
+
+INSERT [dbo].[TableFood] ([id], [name], [status]) VALUES (2, N'Bàn 0', N'Trống')
+INSERT [dbo].[TableFood] ([id], [name], [status]) VALUES (3, N'Bàn 1', N'Trống')
+INSERT [dbo].[TableFood] ([id], [name], [status]) VALUES (4, N'Bàn 2', N'Trống')
+INSERT [dbo].[TableFood] ([id], [name], [status]) VALUES (5, N'Bàn 3', N'Trống')
+INSERT [dbo].[TableFood] ([id], [name], [status]) VALUES (6, N'Bàn 4', N'Trống')
+INSERT [dbo].[TableFood] ([id], [name], [status]) VALUES (7, N'Bàn 5', N'Trống')
+INSERT [dbo].[TableFood] ([id], [name], [status]) VALUES (8, N'Bàn 6', N'Trống')
+INSERT [dbo].[TableFood] ([id], [name], [status]) VALUES (9, N'Bàn 7', N'Trống')
+INSERT [dbo].[TableFood] ([id], [name], [status]) VALUES (10, N'Bàn 8', N'Trống')
+INSERT [dbo].[TableFood] ([id], [name], [status]) VALUES (11, N'Bàn 9', N'Trống')
+INSERT [dbo].[TableFood] ([id], [name], [status]) VALUES (12, N'Bàn 10', N'Trống')
+INSERT [dbo].[TableFood] ([id], [name], [status]) VALUES (13, N'Bàn 11', N'Trống')
+INSERT [dbo].[TableFood] ([id], [name], [status]) VALUES (14, N'Bàn 12', N'Trống')
+INSERT [dbo].[TableFood] ([id], [name], [status]) VALUES (15, N'Bàn 13', N'Trống')
+INSERT [dbo].[TableFood] ([id], [name], [status]) VALUES (16, N'Bàn 14', N'Trống')
+INSERT [dbo].[TableFood] ([id], [name], [status]) VALUES (17, N'Bàn 15', N'Có người')
+INSERT [dbo].[TableFood] ([id], [name], [status]) VALUES (18, N'Bàn 16', N'Trống')
+INSERT [dbo].[TableFood] ([id], [name], [status]) VALUES (19, N'Bàn 17', N'Trống')
+INSERT [dbo].[TableFood] ([id], [name], [status]) VALUES (20, N'Bàn 18', N'Trống')
+INSERT [dbo].[TableFood] ([id], [name], [status]) VALUES (21, N'Bàn 19', N'Trống')
+INSERT [dbo].[TableFood] ([id], [name], [status]) VALUES (22, N'Bàn 20', N'Trống')
+SET IDENTITY_INSERT [dbo].[TableFood] OFF
+ALTER TABLE [dbo].[Bill]  WITH CHECK ADD FOREIGN KEY([idTable])
+REFERENCES [dbo].[TableFood] ([id])
+GO
+ALTER TABLE [dbo].[BillInfo]  WITH CHECK ADD FOREIGN KEY([idBill])
+REFERENCES [dbo].[Bill] ([id])
+GO
+ALTER TABLE [dbo].[BillInfo]  WITH CHECK ADD FOREIGN KEY([idFood])
+REFERENCES [dbo].[Food] ([id])
+GO
+ALTER TABLE [dbo].[Food]  WITH CHECK ADD FOREIGN KEY([idCategory])
+REFERENCES [dbo].[FoodCategory] ([id])
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROC [dbo].[USP_GetAccountByUserName]
+@UserName nvarchar(100)
+AS
+BEGIN
+	SELECT * FROM dbo.Account WHERE UserName = @UserName
+END
+
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROC [dbo].[USP_GetListBillByDate]
+@checkFrom DATE,
+@checkTo DATE
+AS
+BEGIN
+	SELECT t.name AS [Tên bàn],b.DateCheckIn AS [Giờ vào],b.DateCheckOut AS [Giờ ra],b.discount AS [Giảm giá],b.totalPrice AS [Tổng tiền] FROM dbo.Bill AS b,dbo.TableFood AS t
+	WHERE b.idTable = t.id
+		AND	b.DateCheckOut >= @checkFrom
+		AND b.DateCheckOut <= @checkTo
+		AND b.status = '1'
+END
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+ CREATE PROC [dbo].[USP_GetTableList]
+ AS SELECT * FROM dbo.TableFood
+
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROC [dbo].[USP_InsertBill]
+@idTable INT
+AS
+BEGIN
+	INSERT dbo.Bill 
+	        ( DateCheckIn ,
+	          DateCheckOut ,
+	          idTable ,
+	          status,
+	          discount
+	        )
+	VALUES  ( GETDATE() , -- DateCheckIn - date
+	          NULL , -- DateCheckOut - date
+	          @idTable , -- idTable - int
+	          0,  -- status - int
+	          0
+	        )
+END
+
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROC [dbo].[USP_InsertBillInfo]
+@idBill int, @idFood int,@count int
+AS
+BEGIN
+	DECLARE @isExistBillInfo INT
+	DECLARE @foodCount INT = 0 
+	SELECT @isExistBillInfo=b.id ,@foodCount=b.count FROM dbo.BillInfo AS b WHERE idBill = @idBill AND idFood = @idFood
+	IF(@isExistBillInfo>0)
+	BEGIN
+		DECLARE @newCount INT = @foodCount + @count
+		IF(@newCount > 0)
+			UPDATE dbo.BillInfo SET count = @foodCount + @count WHERE idBill = @idBill AND idFood = @idFood
+		ELSE
+			DELETE dbo.BillInfo WHERE idBill = @idBill AND idFood = @idFood
+	END
+	ELSE
+    BEGIN
+    	INSERT INTO dbo.BillInfo
+	        ( idBill, idFood, count )
+	VALUES  ( @idBill, -- idBill - int
+	          @idFood, -- idFood - int
+	          @count  -- count - int
+	          )
+    END
+	
+END
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROC [dbo].[USP_Login]
+@userName nvarchar(100), @passWord nvarchar(100)
+AS
+BEGIN
+	SELECT * FROM dbo.Account WHERE UserName = @userName AND PassWord = @passWord
+END
+
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROC [dbo].[USP_SwitchTabel]
+@idTable1 INT, @idTable2 int
+AS BEGIN
+
+	DECLARE @idFirstBill int
+	DECLARE @idSeconrdBill INT
+	
+	DECLARE @isFirstTablEmty INT = 1
+	DECLARE @isSecondTablEmty INT = 1
+	
+	
+	SELECT @idSeconrdBill = id FROM dbo.Bill WHERE idTable = @idTable2 AND status = 0
+	SELECT @idFirstBill = id FROM dbo.Bill WHERE idTable = @idTable1 AND status = 0
+	
+	IF (@idFirstBill IS NULL)
+	BEGIN
+		INSERT dbo.Bill
+		        ( DateCheckIn ,
+		          DateCheckOut ,
+		          idTable ,
+		          status
+		        )
+		VALUES  ( GETDATE() , -- DateCheckIn - date
+		          NULL , -- DateCheckOut - date
+		          @idTable1 , -- idTable - int
+		          0  -- status - int
+		        )
+		        
+		SELECT @idFirstBill = MAX(id) FROM dbo.Bill WHERE idTable = @idTable1 AND status = 0
+		
+	END
+	
+	SELECT @isFirstTablEmty = COUNT(*) FROM dbo.BillInfo WHERE idBill = @idFirstBill
+	
+	IF (@idSeconrdBill IS NULL)
+	BEGIN
+		INSERT dbo.Bill
+		        ( DateCheckIn ,
+		          DateCheckOut ,
+		          idTable ,
+		          status
+		        )
+		VALUES  ( GETDATE() , -- DateCheckIn - date
+		          NULL , -- DateCheckOut - date
+		          @idTable2 , -- idTable - int
+		          0  -- status - int
+		        )
+		SELECT @idSeconrdBill = MAX(id) FROM dbo.Bill WHERE idTable = @idTable2 AND status = 0
+		
+	END
+	
+	SELECT @isSecondTablEmty = COUNT(*) FROM dbo.BillInfo WHERE idBill = @idSeconrdBill
+
+	SELECT id INTO IDBillInfoTable FROM dbo.BillInfo WHERE idBill = @idSeconrdBill
+	
+	UPDATE dbo.BillInfo SET idBill = @idSeconrdBill WHERE idBill = @idFirstBill
+	
+	UPDATE dbo.BillInfo SET idBill = @idFirstBill WHERE id IN (SELECT * FROM IDBillInfoTable)
+	
+	DROP TABLE IDBillInfoTable
+	
+	IF (@isFirstTablEmty = 0)
+		UPDATE dbo.TableFood SET status = N'Trống' WHERE id = @idTable2
+		
+	IF (@isSecondTablEmty= 0)
+		UPDATE dbo.TableFood SET status = N'Trống' WHERE id = @idTable1
+END
+
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TRIGGER [dbo].[UTG_UpdateBill]
+ON [dbo].[Bill] FOR UPDATE
+AS
+BEGIN
+	DECLARE @idBill INT
+
+	SELECT @idBill = id FROM Inserted
+
+	DECLARE @idTable INT 
+
+	SELECT @idTable = idTable FROM dbo.Bill WHERE id = @idBill
+
+	DECLARE @count INT =0
+	
+	SELECT @count = COUNT(*) FROM dbo.Bill WHERE idTable = @idTable AND status = 0
+
+	IF(@count=0)
+		UPDATE dbo.TableFood SET status = N'Trống' WHERE id = @idTable
+END
+
+GO
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TRIGGER [dbo].[UTG_UpdateBillInfo]
+ON [dbo].[BillInfo] FOR INSERT, UPDATE
+AS 
+BEGIN
+	DECLARE @idBill INT
+	SELECT @idBill = idBill FROM Inserted
+	DECLARE @idTable INT 
+	SELECT @idTable = idTable FROM dbo.Bill WHERE id = @idBill AND status ='0'
+	DECLARE @count INT 
+	SELECT @count = COUNT(*) FROM dbo.BillInfo WHERE idBill = @idBill
+
+	IF(@count > 0)
+	BEGIN
+		UPDATE dbo.TableFood SET status = N'Có người' WHERE id = @idTable
+	END
+	ELSE
+    BEGIN
+    	UPDATE dbo.TableFood SET status = N'Trống' WHERE id = @idTable
+    END
+	
+END
+GO
